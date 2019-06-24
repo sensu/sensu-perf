4a88c5883ef11a8c05c2b4b960112aa75084be00
Sean Porter <portertech@gmail.com>
2019-06-24 09:30:56 -0700

    first experiment, 3 backends, 20k agents, 4 subs, postgres, 4 checks
    Signed-off-by: Sean Porter <portertech@gmail.com>

---
+++ experiments/3-backends-20k-agents-4-subs-pg/loadit2.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+/root/go/bin/loadit -backends ws://192.168.1.15:8081,ws://192.168.1.16:8081,ws://192.168.1.17:8081 \
+-subscriptions large_cluster_test_2 \
+-count 5000 \
+-keepalive-interval 60 \
+-keepalive-timeout 360
