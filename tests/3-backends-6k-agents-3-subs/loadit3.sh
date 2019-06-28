7e0e0f524d43523e7780a93b485c466790e2ac59
Sean Porter <portertech@gmail.com>
2019-06-28 11:29:51 -0700

    fixed 3-backends-6k-agents-3-subs/loadit3.sh agent count, 3k -> 2k
    Signed-off-by: Sean Porter <portertech@gmail.com>

--- tests/3-backends-6k-agents-3-subs/loadit3.sh
+++ tests/3-backends-6k-agents-3-subs/loadit3.sh
@@ -2,6 +2,6 @@
 
 /root/go/bin/loadit -backends ws://192.168.1.15:8081,ws://192.168.1.16:8081,ws://192.168.1.17:8081 \
 -subscriptions large_cluster_test_3 \
--count 3000 \
+-count 2000 \
 -keepalive-interval 60 \
 -keepalive-timeout 360
