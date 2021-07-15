26b0f957756351fe50b9607ff1a7c6c3eb4f10fc
Eric Chlebek <eric@sensu.io>
2021-07-15 13:50:16 -0700

    Fix loadit defn
    Signed-off-by: Eric Chlebek <eric@sensu.io>

--- tests/3-backends-900-agents-30-subs-pg-rr/loadit.sh
+++ tests/3-backends-900-agents-30-subs-pg-rr/loadit.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
-if [ -z "$NUM" ]; then
-	echo '$NUM not defined' && exit 1;
-fi
-
 /root/go/bin/loadit -backends ws://192.168.1.15:8081,ws://192.168.1.16:8081,ws://192.168.1.17:8081 \
--subscriptions "round-robin-${NUM}" \
--count 30 \
+-subscriptions "round-robin" \
+-count 300 \
 -keepalive-interval 60 \
 -keepalive-timeout 360 \
 -pprof-port 6060 \
 -prom=''
-
