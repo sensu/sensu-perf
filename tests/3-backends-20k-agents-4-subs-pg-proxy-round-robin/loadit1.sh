cc7a28d661fe929eb0c15b754886c333378166b6
Eric Chlebek <eric@sensu.io>
2022-01-11 13:18:55 -0800

    Add proxy round robin test
    Signed-off-by: Eric Chlebek <eric@sensu.io>

---
+++ tests/3-backends-20k-agents-4-subs-pg-proxy-round-robin/loadit1.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+/root/go/bin/loadit -backends ws://192.168.1.15:8081,ws://192.168.1.16:8081,ws://192.168.1.17:8081 \
+-subscriptions large_cluster_test_1 \
+-count 5000 \
+-keepalive-interval 60 \
+-keepalive-timeout 360 \
+-pprof-port 6060 \
+-prom "" \
+-base-entity-name "loadit-1-agent"
