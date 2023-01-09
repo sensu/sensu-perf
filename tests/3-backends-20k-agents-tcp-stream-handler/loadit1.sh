317367d9298866ceb3c6c65068ef398e733a05ff
Sean Porter <portertech@gmail.com>
2023-01-09 11:54:32 -0800

    updated network, 192.168.1 -> 10.0.42 /24
    Signed-off-by: Sean Porter <portertech@gmail.com>

--- tests/3-backends-20k-agents-tcp-stream-handler/loadit1.sh
+++ tests/3-backends-20k-agents-tcp-stream-handler/loadit1.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-/root/go/bin/loadit -backends ws://192.168.1.15:8081,ws://192.168.1.16:8081,ws://192.168.1.17:8081 \
+/root/go/bin/loadit -backends ws://10.0.42.15:8081,ws://10.0.42.16:8081,ws://10.0.42.17:8081 \
 -subscriptions large_cluster_test_1 \
 -count 5000 \
 -keepalive-interval 60 \
