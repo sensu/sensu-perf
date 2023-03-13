03cfeb6017364c1c8fb7fc8797657f4f0627d411
Sean Porter <portertech@gmail.com>
2023-03-13 15:02:57 -0700

    use backend dns for loadits
    Signed-off-by: Sean Porter <portertech@gmail.com>

--- tests/3-backends-20k-agents-tcp-stream-handler/loadit4.sh
+++ tests/3-backends-20k-agents-tcp-stream-handler/loadit4.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-/root/go/bin/loadit -backends ws://10.0.42.15:8081,ws://10.0.42.16:8081,ws://10.0.42.17:8081 \
+/root/go/bin/loadit -backends ws://backend1:8081,ws://backend2:8081,ws://backend3:8081 \
 -subscriptions large_cluster_test_4 \
 -count 5000 \
 -keepalive-interval 60 \
